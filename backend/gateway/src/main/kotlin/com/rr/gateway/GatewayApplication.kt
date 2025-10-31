6cf60c78b677f4bde52c7a2b759e62a747a0f84f
qjatn4793 <qjatn4793@naver.com>
2025-10-31 10:55:05 +0900

    GatewayApplication.kt 리턴 타입 변경

--- backend/gateway/src/main/kotlin/com/rr/gateway/GatewayApplication.kt
+++ backend/gateway/src/main/kotlin/com/rr/gateway/GatewayApplication.kt
@@ -6,4 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class GatewayApplication
 
-fun main(args: Array<String>) = runApplication<GatewayApplication>(*args)
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+}
